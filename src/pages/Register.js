e1dbbd2c5899fe8bfa0db9dea9567ff16352a853
hebaarafat159 <heba.arafat159@gmail.com>
2024-01-29 23:18:29 +0000

    validate form inputs

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -12,13 +12,11 @@ export default function Register() {
   const [parentLastName, setParentLastName] = useState('')
   const [email, setEmail] = useState('')
   const [mobileNumber, setMobileNumber] = useState('')
-  const [programType, setProgramType] = useState('')
-  const [location, setLocation] = useState('')
+  const [program_type, setprogram_type] = useState('')
+  const [preffered_location, setpreffered_location] = useState('')
   const [hearAboutUs, setHearAboutUs] = useState('')
   const [questions, setQuestions] = useState('')
   const [children, setChildren] = useState([])
-  // eslint-disable-next-line
-  const [addNewChild, setAddNewChild] = useState(false)
 
   const [child, setChild] = useState({
     first_name: '',
@@ -26,6 +24,125 @@ export default function Register() {
     age: 0,
     hasComputer: false
   });
+
+  const [errors, setErrors] = useState({
+    parentFirstName: '',
+    parentLastName: '',
+    email: '',
+    mobileNumber: '',
+    programType: '',
+    preffered_location: '',
+    hearAboutUs: '',
+    questions: ''
+  });
+
+  const validateForm = () => {
+    let valid = true;
+    const newErrors = { ...errors };
+
+    // Validate parent first name
+    if (parentFirstName.trim() === '') {
+      newErrors.parentFirstName = 'Parent first name is required';
+      valid = false;
+    } else {
+      newErrors.parentFirstName = '';
+    }
+
+    // Validate parent last name
+    if (parentLastName.trim() === '') {
+      newErrors.parentLastName = 'Parent Last Name is required';
+      valid = false;
+    } else {
+      newErrors.parentLastName = '';
+    }
+
+    // Validate email
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (email.trim() === '') {
+      newErrors.email = 'Email is required';
+      valid = false;
+    }
+    else if (!emailRegex.test(email.trim())) {
+      newErrors.email = 'Invalid email format';
+      valid = false;
+    } else {
+      newErrors.email = '';
+    }
+
+    // validate mobile number
+    const mobileRegex = /^\d{10}$/; // Assumes a 10-digit mobile number
+    if (email.trim() === '') {
+      newErrors.mobileNumber = 'Mobile is required';
+      valid = false;
+    }
+    else if (!mobileRegex.test(mobileNumber.trim())) {
+      newErrors.mobile = 'Invalid mobile number';
+      valid = false;
+    } else {
+      newErrors.mobile = '';
+    }
+
+    // Update the errors state
+    setErrors(newErrors);
+
+    return valid;
+  }
+
+  function submit() {
+    // event.preventDefault();
+
+    // Validate the form
+    if (validateForm()) {
+      // Form is valid, you can proceed with form submission or other actions
+      //TODO handle send message
+      alert
+        (
+          `parent: ${parentFirstName} ${parentLastName},
+      email: ${email},
+      Mobile: ${mobileNumber},
+      Program Type: ${program_type},
+      preffered_location: ${preffered_location},
+      child: ${child.first_name} ${child.last_name} , age: ${child.age}, Has Computer: ${child.hasComputer},
+      Hear About Us : ${hearAboutUs},
+      Questions : ${questions}`,
+        )
+      children.push(child)
+      const childrenStr = JSON.stringify(children)
+      console.log(`childrenStr : ${childrenStr}`)
+      // fetch(`http://localhost:4000/classrooms/register`,
+      // fetch(`${process.env.REACT_APP_URL_APP_PATH}/classrooms/register`,
+      //   {
+      //     method: "POST",
+      //     headers: {
+      //       "Content-Type": "application/json"
+      //     },
+      //     body: JSON.stringify({
+      //       "first_name": parentFirstName,
+      //       "last_name": parentLastName,
+      //       "email": email,
+      //       "mobile": mobileNumber,
+      //       "hear_about_us": hearAboutUs,
+      //       "questions": questions,
+      //       "children": children,
+      //       "preffered_location": preffered_location,
+      //       "program_type": program_type,
+      //       "course_id": ""
+      //     })
+      //   })
+      //   .then(response => response.json())
+      //   .then(result => {
+      //     if (result.status === 200) {
+      //       console.log(`YOur Registration has been send to MINI developer, one of our team memeber will answer you shortly`);
+      //     }
+      //   })
+      //   .catch(error => {
+      //     console.log(error);
+      //   })
+    } else {
+      // Form is not valid, display error messages or take other actions
+      console.log('Form validation failed');
+    }
+  }
 
   function updateChildProperty(propertyName, newValue) {
     setChild((prevObject) => ({
@@ -46,53 +163,6 @@ export default function Register() {
     }
   }
 
-  function submit() {
-    //TODO handle send message
-    alert
-      (
-        `parent: ${parentFirstName} ${parentLastName},
-        email: ${email},
-        Mobile: ${mobileNumber},
-        Program Type: ${programType},
-        LOcation: ${location},
-        child: ${child.firstName} ${child.lastName} , age: ${child.age}, Has Computer: ${child.hasComputer},
-        Hear About Us : ${hearAboutUs},
-        Questions : ${questions}`
-      )
-    children.push(child)
-    const childrenStr = JSON.stringify(children)
-    console.log(`childrenStr : ${childrenStr}`)
-    // fetch(`http://localhost:4000/classrooms/register`,
-    fetch(`${process.env.REACT_APP_URL_APP_PATH}/classrooms/register`,
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-          "first_name": parentFirstName,
-          "last_name": parentLastName,
-          "email": email,
-          "mobile": mobileNumber,
-          "hear_about_us": hearAboutUs,
-          "questions": questions,
-          "children": children,
-          "location": location,
-          "program_type": programType,
-          "course_id": ""
-        })
-      })
-      .then(response => response.json())
-      .then(result => {
-        if (result.status === 200) {
-          console.log(`YOur Registration has been send to MINI developer, one of our team memeber will answer you shortly`);
-        }
-      })
-      .catch(error => {
-        console.log(error);
-      })
-  }
-
   return (
     <Card className={cssStyle.page}>
       <Stack spacing={1} className={cssStyle.register_form_component} >
@@ -110,7 +180,8 @@ export default function Register() {
               label="First Name"
               defaultValue={parentFirstName}
               onChange={(event) => setParentFirstName(event.target.value)}
-
+              error={Boolean(errors.parentFirstName)}
+              helperText={errors.parentFirstName}
             />
             <TextField
               required
@@ -118,6 +189,8 @@ export default function Register() {
               label="Last Name"
               defaultValue={parentLastName}
               onChange={(event) => setParentLastName(event.target.value)}
+              error={Boolean(errors.parentLastName)}
+              helperText={errors.parentLastName}
             />
           </Stack>
 
@@ -131,6 +204,8 @@ export default function Register() {
             defaultValue={email}
             className={cssStyle.register_form_text_field}
             onChange={(event) => setEmail(event.target.value)}
+            error={Boolean(errors.email)}
+            helperText={errors.email}
           />
 
           {/* Mobile Number */}
@@ -143,6 +218,8 @@ export default function Register() {
             defaultValue={mobileNumber}
             className={cssStyle.register_form_text_field}
             onChange={(event) => setMobileNumber(event.target.value)}
+            error={Boolean(errors.email)}
+            helperText={errors.mobileNumber}
           />
 
           {/* add new child layout */}
@@ -165,7 +242,7 @@ export default function Register() {
             addToChildren={addToChildren} />
 
           {/* program type In=person or Online */}
-          <ProgrameTypeComponent location={location} setLocation={setLocation} programType={programType} setProgramType={setProgramType} />
+          <ProgrameTypeComponent preffered_location={preffered_location} setpreffered_location={setpreffered_location} program_type={program_type} setprogram_type={setprogram_type} />
 
           {/* hear about us */}
           <Typography style={{ fontSize: '1.5vw', color: '#333440' }}> How did you hear about us? </Typography>
