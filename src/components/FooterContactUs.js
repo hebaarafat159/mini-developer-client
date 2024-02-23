33f8d3205e66e9df063203c0a8edf85424513577
hebaarafat159 <heba.arafat159@gmail.com>
2024-02-23 00:02:44 +0000

    modify contact us form

--- src/components/FooterContactUs.js
+++ src/components/FooterContactUs.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import { SocialIcon } from 'react-social-icons'
-import { Stack } from '@mui/material'
+import { Stack, Typography } from '@mui/material'
 
 export default function FooterContactUs() {
     return (
         <Stack direction="row" spacing={1} style={{ display: 'flex', justifyContent: 'center' }}>
+            <Typography style={{ display: 'flex', justifyContent: 'center', alignItems:'center' }}>Contact Us</Typography>
             <SocialIcon network="facebook" url='https://www.facebook.com/MiniDeveloperUK' />
             <SocialIcon network="whatsapp" url='+447851496016' />
             <SocialIcon network="instagram" url='https://www.instagram.com/minideveloperuk' />
