843995f32eda96220fa819410519dc93610a06e6
hebaarafat159 <heba.arafat159@gmail.com>
2024-02-23 00:37:56 +0000

    modify contact us

--- src/components/FooterContactUs.js
+++ src/components/FooterContactUs.js
@@ -5,7 +5,7 @@ import { Stack, Typography } from '@mui/material'
 export default function FooterContactUs() {
     return (
         <Stack direction="row" spacing={1} style={{ display: 'flex', justifyContent: 'center' }}>
-            <Typography style={{ display: 'flex', justifyContent: 'center', alignItems:'center' }}>Contact Us</Typography>
+            <Typography style={{ display: 'flex', alignItems:'center' }}>Contact Us</Typography>
             <SocialIcon network="facebook" url='https://www.facebook.com/MiniDeveloperUK' />
             <SocialIcon network="whatsapp" url='+447851496016' />
             <SocialIcon network="instagram" url='https://www.instagram.com/minideveloperuk' />
