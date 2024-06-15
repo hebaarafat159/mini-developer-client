45972b586b67e3bf249c0f9cf93dcd6f57769697
hebaarafat159 <heba.arafat159@gmail.com>
2024-06-15 22:46:08 +0100

    Fix Mobile view in Why us screen

--- src/components/WhyUsComponent.js
+++ src/components/WhyUsComponent.js
@@ -3,51 +3,46 @@ import { Stack, Typography } from '@mui/material'
 import FreeTrialButton from './FreeTrialButton'
 import cssStyle from '../css/styles.module.css'
 
+import why_img from '../assets/why-learn-coding-at-a-young-age-mini-coding-coding-for-kids-life-fun.gif'
 export default function WhyUsComponent() {
     return (
-        <Stack spacing={1} className={cssStyle.page} >
-            <Typography style={{ fontWeight: 'bold', fontSize: '2vw', color: '#ed7d45' }} > Why Choose Mini Developer? </Typography>
-            <Stack spacing={1} >
-                <ul>
-                    <li>
-                        <Typography style={{ fontWeight: 'normal', fontSize: '1.2vw', color: '#333440', padding: '2vmin 0' }}>
-                            Sessions are run by qualified experienced <span style={{ fontWeight: 'bold' }}> Software Developers </span>.
-                        </Typography>
-                    </li>
-                    <li>
-                        <Typography style={{ fontWeight: 'normal', fontSize: '1.2vw', color: '#333440', padding: '2vmin 0' }}>
-                            All tutors are<span style={{ fontWeight: 'bold' }}> DBS </span>checked.
-                        </Typography>
-                    </li>
-                    <li>
-                        <Typography style={{ fontWeight: 'normal', fontSize: '1.2vw', color: '#333440', padding: '2vmin 0' }}>
-                            All our staff is trained for children<span style={{ fontWeight: 'bold' }}> safeguarding</span>.
-                        </Typography>
-                    </li>
-                    <li>
-                        <Typography style={{ fontWeight: 'normal', fontSize: '1.2vw', color: '#333440', padding: '2vmin 0' }}>
-                            Our interactive program enables children to learn<span style={{ fontWeight: 'bold' }}>  coding with fun</span>.
-                        </Typography>
-                    </li>
-                    <li>
-                        <Typography style={{ fontWeight: 'normal', fontSize: '1.2vw', color: '#333440', padding: '2vmin 0' }}>
-                            Our students gain greater confidence in technology and coding.
-                        </Typography>
-                    </li>
-                    <li>
-                        <Typography style={{ fontWeight: 'normal', fontSize: '1.2vw', color: '#333440', padding: '2vmin 0' }}>
-                            We provide <span style={{ fontWeight: 'bold' }}>in-person</span> lessons and online lessons to suit all needs.
-                        </Typography>
-                    </li>
-                    <li>
-                        <Typography style={{ fontWeight: 'normal', fontSize: '1.2vw', color: '#333440', padding: '2vmin 0' }}>
-                            Accomplishment <span style={{ fontWeight: 'bold' }}>Certificates presented</span>.
-                        </Typography>
-                    </li>
-                </ul>
-            </Stack>
-            <FreeTrialButton course={null} />
-        </Stack>
+        <Stack direction="row" spacing={2} sx={{ justifyContent: 'space-evenly', alignItems: 'center', background: '#333440', padding: '2vw' }} >
+            <Stack direction="column" spacing={2} sx={{ justifyContent: 'center', alignItems: 'center', background: 'white', padding: '3vw' }} >
+                <Typography component="h4" variant='h6' style={{ color: '#ed7d45', textAlign: 'center', fontWeight: 'bold' }}>
+                    Build your Child's Valuable Coding & Problem-Solving Skills
+                </Typography>
 
+                <Typography component="p" variant='p' style={{ color: '#333440' }}>
+                    * Sessions are run by qualified experienced <span style={{ fontWeight: 'bold' }}> Software Developers </span>.
+                </Typography>
+
+                <Typography component="p" variant='p' style={{ color: '#333440' }}>
+                    * All tutors are<span style={{ fontWeight: 'bold' }}> DBS </span>checked. <span style={{ fontWeight: 'bold' }}> Software Developers </span>.
+                </Typography>
+
+                <Typography component="p" variant='p' style={{ color: '#333440' }}>
+                    * All our staff is trained for children<span style={{ fontWeight: 'bold' }}> safeguarding</span>.
+                </Typography>
+
+                <Typography component="p" variant='p' style={{ color: '#333440' }}>
+                    * Our interactive program enables children to learn<span style={{ fontWeight: 'bold' }}>  coding with fun</span>.
+                </Typography>
+
+                <Typography component="p" variant='p' style={{ color: '#333440' }}>
+                    * Our students gain greater confidence in technology and coding.
+                </Typography>
+
+                <Typography component="p" variant='p' style={{ color: '#333440' }}>
+                    * We provide <span style={{ fontWeight: 'bold' }}>in-person</span> lessons and online lessons to suit all needs.
+                </Typography>
+
+                <Typography component="p" variant='p' style={{ color: '#333440' }}>
+                    * Accomplishment <span style={{ fontWeight: 'bold' }}>Certificates presented</span>.
+                </Typography>
+                <FreeTrialButton course={null} />
+            </Stack>
+            {/* <img src={why_img} loading="lazy" alt="" width={'30%'} /> */}
+
+        </Stack>
     )
 }
