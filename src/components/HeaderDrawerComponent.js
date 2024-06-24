e073017a90f89472cb7b02ad5c73b04a37706ba4
hebaarafat159 <heba.arafat159@gmail.com>
2024-06-24 03:08:13 +0100

    fix header menu item style

--- src/components/HeaderDrawerComponent.js
+++ src/components/HeaderDrawerComponent.js
@@ -2,19 +2,30 @@ import React, { useState } from 'react'
 import { Drawer, IconButton, List, ListItemButton, ListItemIcon, ListItemText, Link } from '@mui/material'
 import MenuIcon from '@mui/icons-material/Menu';
 import cssStyle from '../css/styles.module.css'
+import '../App.css'
 
 export default function HeaderDrawerComponent({ pages, handleSelected }) {
     const [openDrawer, setOpenDrawer] = useState(false)
     return (
         <React.Fragment>
             <Drawer anchor="right" open={openDrawer} onClose={() => setOpenDrawer(false)}>
-                <List>
+                <List sx={{
+                    width: '30%',  // Example width
+                    padding: '20px',
+                    variant: 'h6',
+                    fontWeight: 'bold',
+                    color: '#4F8DB9'
+                }}>
                     {
                         pages.map((page, index) => (
-                            <ListItemButton onClick={() => {
-                                setOpenDrawer(false)
-                                handleSelected(page)
-                            }} key={index} component={Link} to={page.link}>
+                            <ListItemButton
+                                variant="h6"
+                                className={`link_text ${page.isSelected ? 'selected' : ''}`}
+                                style={{ textDecoration: 'none', fontWeight: 'bold', padding: '2vmin', fontStyle: 'italic' }}
+                                onClick={() => {
+                                    setOpenDrawer(false)
+                                    handleSelected(page)
+                                }} key={index} component={Link} to={page.link}>
                                 <ListItemIcon>
                                     <ListItemText >{page.label}</ListItemText>
                                 </ListItemIcon>
--- src/components/HeaderMenuTabs.js
+++ src/components/HeaderMenuTabs.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link, Box } from '@mui/material'
-import cssStyle from '../css/styles.module.css'
+// import cssStyle from '../css/styles.module.css'
+import '../App.css'
 
 export default function HeaderMenuTabs({ pages, handleSelected }) {
     return (
@@ -8,10 +9,14 @@ export default function HeaderMenuTabs({ pages, handleSelected }) {
             {
                 pages.map((page, index) => (
                     <Link key={page.position}
-                        className={cssStyle.link_text}
+                        // className={cssStyle.link_text}
+                        variant="h6"
+                        className={`link_text ${page.isSelected ? 'selected' : ''}`}
+                        // variant='p'
                         // color={(page.isSelected) ? '#ed7d45' : '#333440'}
-                        color='#333440'
-                        href={page.link} style={{ textDecoration: 'none', fontWeight: 'bold', fontSize: '1.2vw' }}
+                        // color='#333440'
+                        href={page.link}
+                        style={{ textDecoration: 'none', fontWeight: 'bold', padding: '2vmin', fontStyle: 'italic' }}
                         onClick={() => {
                             handleSelected(page)
                         }}>
