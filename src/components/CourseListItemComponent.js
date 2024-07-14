c855f56940e7d3ebdaa59d3829e16b0680a6ee3a
hebaarafat159 <heba.arafat159@gmail.com>
2024-07-14 22:02:47 +0100

    modify css

--- src/components/CourseListItemComponent.js
+++ src/components/CourseListItemComponent.js
@@ -1,30 +1,29 @@
 import React from 'react'
-import cssStyle from '../css/styles.module.css'
 import { Card, Stack, Typography, Button } from '@mui/material'
 import { Link } from 'react-router-dom'
 
 export default function CourseListComponent({ course }) {
   return (
-    <Card className={cssStyle.course_card}>
+    <Card className={'course_card'}>
       <div
-        className={cssStyle.course_card_image}
+        className={'course_card_image'}
         style={{ backgroundImage: `url("${course.cover_image}")` }}>
         <Link to={{ pathname: `/courses/${course._id}` }}>
           <Button variant="contained" style={{
             borderRadius: 10,
             background: 'linear-gradient(to bottom, #ffb093, #ed7d45)',
             fontSize: "2.5vmin",
-            marginBottom:0
+            marginBottom: 0
           }}>
             View Details
           </Button>
         </Link>
       </div>
 
-      <Stack className={cssStyle.course_card_details}>
-        <Typography component="p" variant='p' style={{ color: '#333440', fontWeight:'bolder' }}>{course.title}</Typography>
-        <Typography component="p" variant='p' style={{ color: '#333440' }}> <span className={cssStyle.course_card_bold_text}>Age:</span> {course.age} </Typography>
-        <Typography component="p" variant='p' style={{ color: '#333440' }}> <span className={cssStyle.course_card_bold_text}>Language:</span> {course.language}</Typography>
+      <Stack className={'course_card_details'}>
+        <Typography component="p" variant='p' style={{ color: '#333440', fontWeight: 'bolder' }}>{course.title}</Typography>
+        <Typography component="p" variant='p' style={{ color: '#333440' }}> <span className={'course_card_bold_text'}>Age:</span> {course.age} </Typography>
+        <Typography component="p" variant='p' style={{ color: '#333440' }}> <span className={'course_card_bold_text'}>Language:</span> {course.language}</Typography>
       </Stack>
     </Card >
   )
