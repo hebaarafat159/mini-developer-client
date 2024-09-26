c110ecb12b460f136976408c2c1c613b32a0227d
hebaarafat159 <heba.arafat159@gmail.com>
2024-09-26 02:47:21 +0100

    modify confimation page

--- src/pages/ConfirmationPage.js
+++ src/pages/ConfirmationPage.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Stack, Typography, Button, Card } from '@mui/material'
+import { Stack, Typography, Button } from '@mui/material'
 
 export default function ConfirmationPage() {
 
@@ -7,13 +7,16 @@ export default function ConfirmationPage() {
     document.title = ' Confirmation | Mini Developer';
 
     return (
-        <Card className='recent-blogs d-block'>
-            <Stack direction="column" spacing={2} sx={{ justifyContent: 'center', background: 'white', padding: '3vw' }} >
-                <Typography component="h3" variant='h5' style={{ color: '#4682b4', textAlign: 'center', fontWeight: 'bold' }}>Your Registration has been send to MINI developer, one of our team memeber will answer you shortly </Typography>
+        <Stack className='recent-blogs d-block' sx={{ justifyContent: 'center', alignItems: 'center', background: 'white', padding: '3vw' }}>
+            <Stack direction="column" spacing={2} sx={{ justifyContent: 'center', alignItems: 'center', background: 'white', padding: '3vw' }} >
+                <Stack direction="row" width={'80%'} sx={{ justifyContent: 'center', alignItems: 'center', background: 'white', padding: '3vw' }} >
+                    <img src={'https://drive.google.com/thumbnail?id=1k7zjHa2vZ46pjDxN1nFGea7eVlQ15s5N'} loading="lazy" alt="" style={{ padding: '1vmin' }} />
+                    <Typography component="h3" variant='h5' style={{ color: '#4682b4', textAlign: 'left', fontWeight: 'bold' }}>Thanks for your interest in Mini Developer courses. We received your registration and a member of the team will be in touch with you soon! </Typography>
+                </Stack>
                 <Button variant="contained"
                     className={'orage_btn'}
                     LinkComponent={'a'} href={'/'}> Back To Home </Button>
             </Stack>
-        </Card>
+        </Stack>
     )
 }
