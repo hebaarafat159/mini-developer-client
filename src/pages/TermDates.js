af15c31fe78ddc2052051c6a9705c00a25807e0b
hebaarafat159 <heba.arafat159@gmail.com>
2025-02-27 01:45:08 +0000

    fix term dates time line view

--- src/pages/TermDates.js
+++ src/pages/TermDates.js
@@ -1,35 +1,16 @@
-import { Stack, List, Divider, Typography, ListItemDecorator, ListItemContent, Card } from "@mui/joy";
+import { Stack, Divider, Typography, Card } from "@mui/joy";
 import { Grid } from "@mui/material";
 import term_dates_img from '../assets/term_dates_img.png'
 import SEOComponent from '../components/SEOComponent.js'
 import React, { useEffect, useState, useRef } from 'react'
+import { timelineItemClasses } from '@mui/lab/TimelineItem';
+import { Timeline, TimelineItem, TimelineSeparator, TimelineConnector, TimelineContent, TimelineDot } from "@mui/lab";
 
 
 export default function TermDates() {
     const isFetched = useRef(false);
     const [termDates, setTermDates] = useState([])
     const [currentTerm, setCurrentTerm] = useState({})
-
-    // eslint-disable-next-line 
-    // function setSelectedTerm() {
-    //     termDates.forEach(term => {
-    //         if (term.is_current_term) {
-    //             setCurrentTerm(term);
-    //             // alert("Current Dates: " + JSON.stringify(term));
-    //         }
-    //     });
-    // }
-
-
-    // const setSelectedTerm = useCallback(() => {
-    //     // Logic for setSelectedTerm
-    //     termDates.forEach(term => {
-    //         if (term.is_current_term) {
-    //             setCurrentTerm(term);
-    //             // alert("Current Dates: " + JSON.stringify(term));
-    //         }
-    //     });
-    // }, [termDates]);
 
     useEffect(() => {
         if (!isFetched.current) {
@@ -91,69 +72,52 @@ export default function TermDates() {
             {/* year trem dates sections */}
             <Stack direction="column" spacing={2} sx={{ my: 1, padding: '2vw', flexGrow: 1 }}>
 
+                {/* Academy year section */}
                 <Divider component="div" sx={{ my: 2 }} />
                 <Typography component="h2" variant='h2' style={{ color: '#333440', textAlign: 'center', fontWeight: 'bold' }}>
                     Academic Year {process.env.REACT_APP_CURRENT_TERM_YEAR}
                 </Typography>
                 <Divider component="div" sx={{ my: 2 }} />
 
-                <List sx={{ alignItems: 'center', display: 'flex', justifyContent: 'center' }}>
+                {/*  year time line section */}
+                <Timeline
+                    sx={{
+                        [`& .${timelineItemClasses.root}:before`]: {
+                            flex: 0,
+                            padding: 0,
+                        },
+                    }}
+                >
+
                     {(termDates && termDates.length > 0) ?
                         termDates.map((termDate, index) => (
+                            <TimelineItem>
+                                <TimelineSeparator>
+                                    <TimelineDot color="primary" />
+                                    <TimelineConnector />
+                                </TimelineSeparator>
+                                <TimelineContent>
+                                    <Typography component="h5" variant='h6' style={{ color: '#333440', fontWeight: 'bold', padding: '1vmin auto' }}>{termDate.title}</Typography>
+                                    <Typography component="p" variant='p' style={{ color: '333440' }}>{termDate.duration}</Typography>
+                                    <Card style={{ margin: '1vmin 0' }}>
+                                        {(termDate.sections && termDate.sections.length > 0) ?
+                                            termDate.sections.map((section, index) => (
+                                                <Stack>
+                                                    <Typography component="h5" variant='h6' style={{ color: '#ed7d45', fontWeight: 'bold', textAlign: 'left', padding: '1.5vmin 0' }}>{section.title}</Typography>
+                                                    <Typography component="p" variant='p' style={{ color: '#333440' }}> {section.start_date}</Typography>
 
-                            <Grid container key={index} columns={{ xs: 2, sm: 8, md: 12 }}>
-                                {/* title and duration dates  */}
-                                <Grid item xs={2} >
-                                    <ListItemContent >
-                                        <Typography component="h5" variant='h6' style={{ color: '#333440', fontWeight: 'bold', padding: '1vmin auto' }}>{termDate.title}</Typography>
-                                        <Typography component="p" variant='p' style={{ color: '333440' }}>{termDate.duration}</Typography>
-                                    </ListItemContent>
-                                </Grid>
-                                <Grid container xs={8} >
-                                    <Grid item xs={2} >
-                                        <ListItemDecorator
-                                            sx={{
-                                                '&:before': {
-                                                    content: '""',
-                                                    position: 'absolute',
-                                                    height: '100%',
-                                                    width: '.2vmin',
-                                                    bgcolor: 'divider',
-                                                    // left: '1vmin',
-                                                    top: '2vmin'
-                                                },
-                                                alignItems: 'center',
-                                                display: 'flex',
-                                                justifyContent: 'center'
-                                            }}>
-                                            <svg preserveAspectRatio="none" data-bbox="0 0 200 200" xmlns="http://www.w3.org/2000/svg" width="8%" height="8%" viewBox="0 0 200 200" role="presentation" aria-hidden="true" aria-label="">
-                                                <g>
-                                                    <path d="M200 100c0 55.228-44.772 100-100 100S0 155.228 0 100 44.772 0 100 0s100 44.772 100 100z"></path>
-                                                </g>
-                                            </svg>
-                                        </ListItemDecorator>
-                                    </Grid>
-                                    {/* term dates section */}
-                                    <Grid item xs={8}>
-                                        <Card style={{ margin: '1vmin 0' }}>
-                                            {(termDate.sections && termDate.sections.length > 0) ?
-                                                termDate.sections.map((section, index) => (
-                                                    <Stack>
-                                                        <Typography component="h5" variant='h6' style={{ color: '#ed7d45', fontWeight: 'bold', textAlign: 'left', padding: '1.5vmin 0' }}>{section.title}</Typography>
-                                                        <Typography component="p" variant='p' style={{ color: '#333440' }}> {section.start_date}</Typography>
+                                                    <Typography component="p" variant='p' style={{ color: '#333440', fontWeight: 'bold', textAlign: 'left' }}>{section.end_date}</Typography>
+                                                    <Typography component="p" variant='p' style={{ color: '#333440' }}>{section.description}</Typography>
 
-                                                        <Typography component="p" variant='p' style={{ color: '#333440', fontWeight: 'bold', textAlign: 'left' }}>{section.end_date}</Typography>
-                                                        <Typography component="p" variant='p' style={{ color: '#333440' }}>{section.description}</Typography>
+                                                </Stack>
 
-                                                    </Stack>
-
-                                                )) : null}
-                                        </Card>
-                                    </Grid>
-                                </Grid>
-                            </Grid>
+                                            )) : null}
+                                    </Card>
+                                </TimelineContent>
+                            </TimelineItem>
                         )) : null}
-                </List>
+
+                </Timeline>
 
             </Stack>
         </Stack>
