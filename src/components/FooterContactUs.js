ce33cb10f4dbb6fe410f16b99a5593eb14d8aa11
hebaarafat159 <heba.arafat159@gmail.com>
2025-02-27 00:49:34 +0000

    fix footer logo

--- src/components/FooterContactUs.js
+++ src/components/FooterContactUs.js
@@ -19,7 +19,7 @@ export default function FooterContactUs() {
         <Stack spacing={1} sx={{ display: 'flex', direction: 'row', justifyContent: 'space-around', alignItems: 'center' }} >
             <Grid container columns={{ xs: 2, sm: 8, md: 12 }} sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
                 <Grid item xs={2} sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-                    <img src={app_logo} loading="lazy" alt="" width={'100%'} />
+                    <img src={app_logo} loading="lazy" alt="" width={'70%'} />
                 </Grid>
                 <Grid item xs={8} sx={{ justifyContent: 'center', alignItems: 'center', padding: '2vmin' }}>
                     <Stack direction="row" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
