d2dce2961711d4399db903f553a848049bad6bbb
hebaarafat159 <heba.arafat159@gmail.com>
2025-03-29 02:04:35 +0000

    add icon

--- src/pages/Testimonial2.js
+++ src/pages/Testimonial2.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { Typography, Grid, Card, CardContent, Box, Container, Rating } from '@mui/material'
 import { Masonry } from "@mui/lab";
+import feedback_icon from '../assets/feedback_icon.png'
 // import { Star } from "@mui/icons-material";
 
 export default function Testimonial2({ testimonialsCount }) {
@@ -30,13 +31,14 @@ export default function Testimonial2({ testimonialsCount }) {
         return (
             <Card sx={{ backgroundColor: "white", color: "black", p: 2, borderRadius: 3 }}>
                 <Box display="flex" alignItems="center" gap={2}>
-                    <Box>
+                    <img src={feedback_icon} loading="lazy" alt="" width={'10%'} />
+                    <Box >
                         <Typography variant="h6" fontWeight="bold">
                             {testimonial.person}
                         </Typography>
-                        <Typography variant="body2" color="gray">
+                        {/* <Typography variant="body2" color="gray">
                             {testimonial.course_id.title}
-                        </Typography>
+                        </Typography> */}
                     </Box>
                 </Box>
                 <CardContent>
@@ -79,6 +81,7 @@ export default function Testimonial2({ testimonialsCount }) {
     return (
         // <Container sx={{ mt: 5 }}>
         <Box py={6} sx={{ backgroundColor: "#f9f9f9" }}>
+            <Typography component="h4" variant='h4' style={{ color: '#4682b4', textAlign: 'center', fontWeight: 'bold', padding: '3vmin auto', fontFamily: 'Papyrus' }}> Do you know what our Customers say about us?  </Typography>
             <Container maxWidth="xl" sx={{ mt: 3 }}>
                 <Masonry columns={{ xs: 1, sm: 2, md: 3 }} spacing={2}>
                     {testimonials.map((testimonial, index) => (
@@ -87,20 +90,5 @@ export default function Testimonial2({ testimonialsCount }) {
                 </Masonry>
             </Container>
         </Box>
-        // <Box py={6} sx={{ backgroundColor: "#f9f9f9" }}>
-        //     <Container maxWidth="xl">
-        //         <Typography variant="h4" sx={{ color: "#4682b4", fontWeight: "bold", textAlign: "center", mb: 4 }}>
-        //             Do you know what our Customers say about us?
-        //         </Typography>
-
-        //         <Grid container spacing={2} justifyContent="center">
-        //             {testimonials.map((testimonial, index) => (
-        //                 <Grid item key={index}>
-        //                     <TestimonialCard testimonial={testimonial} />
-        //                 </Grid>
-        //             ))}
-        //         </Grid>
-        //     </Container>
-        // </Box>
     );
 }
