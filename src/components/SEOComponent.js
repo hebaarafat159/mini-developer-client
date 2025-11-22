96dea5e6be81db210bba1141f8a0a03db67faf9e
hebaarafat159 <heba.arafat159@gmail.com>
2025-11-22 00:52:45 +0000

    update SEO description

--- src/components/SEOComponent.js
+++ src/components/SEOComponent.js
@@ -25,26 +25,23 @@ export default function SEOComponent({ seoData }) {
                 <meta name="slug" content={seoData.seo_slug} />
 
                 {/* description -> (description in search results) */}
-                <meta name="description" content={seoData.seo_description} />
+                {/* <meta name="description" content={seoData.seo_description} /> */}
                 {/* og:description -> (description on social media) */}
-                <meta name="description" content={seoData.seo_description} />
+                {/* <meta name="description" content={seoData.seo_description} /> */}
                 {/* X:description -> (description on x) any*/}
+                {/* <meta name="description" content={seoData.seo_description} /> */}
+
+                {/* Search engines */}
                 <meta name="description" content={seoData.seo_description} />
 
+                {/* Social media - Open Graph */}
+                <meta property="og:description" content={seoData.seo_description} />
+
+                {/* Twitter / X */}
+                <meta name="twitter:description" content={seoData.seo_description} />
 
                 <link rel="canonical" />
-                {/* <meta name="description" content="This is a description of my page" />
-         <meta name="keywords" content="react, meta tags, seo" />
-        <meta name="author" content="Your Name" />
-        <meta property="og:title" content="My Page Title" />
-        <meta property="og:description" content="This is a description of my page" />
-        <meta property="og:image" content="https://example.com/image.jpg" />
-        <meta property="og:url" content="https://example.com/my-page" />
-        <meta name="twitter:title" content="My Page Title" />
-        <meta name="twitter:description" content="This is a description of my page" />
-        <meta name="twitter:image" content="https://example.com/image.jpg" />
-        <meta name="twitter:card" content="summary_large_image" />
-       */}
+
             </Helmet>
         )
     )
